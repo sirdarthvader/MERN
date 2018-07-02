5899ebd94242a77acb17706a4c94e60b73e2066a
Ashish Singh <ashishcodes4@gmail.com>
2018-07-02 11:10:53 +0530

    connected to DB and routes setup..

--- MERN1.0/index.js
+++ MERN1.0/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const port = 5000;
 
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profiles = require('./routes/api/profiles');
+
+//config DB..
+const db = require('./config/keys.js').mongoURI;
+
+//Connect to mongoDB...
+mongoose
+    .connect(db)
+    .then(()=> console.log('mongoDB connected'))
+    .catch(err => console.log(err));
+
+//Route Setup...
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
+
+
+//Route Operations...
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 
+
+//Start Server....
 app.listen(port, ()=> {
     `server started on ${port}`
 });
---
+++ MERN1.0/routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/test', (req, res) => {
+    res.json({
+        msg: "posts works"
+    });
+});
+
+module.exports = router;
