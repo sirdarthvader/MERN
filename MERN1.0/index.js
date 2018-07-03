58b0149b6ccb162b2f8118ec3176c3284628c9ba
Ashish Singh <ashishcodes4@gmail.com>
2018-07-03 09:22:45 +0530

    setting up register route

--- MERN1.0/index.js
+++ MERN1.0/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Body Parser config...
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const port = 5000;
 
--- MERN1.0/routes/api/users.js
+++ MERN1.0/routes/api/users.js
@@ -1,10 +1,43 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
+
+// Load user model...
+const User = require('../../models/User');
+
+//@route: /api/users/test
+//@desc : used for testing users route
+//@access: public
 router.get('/test', (req, res) => {
-    res.json({
-        msg: "users works"
-    });
+  res.json({
+    msg: "users works"
+  });
+});
+
+//@route : /api/users/register
+//@desc: used for registering users ion database
+//@access: public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        res.status(400).json({email: "A user with this email already exists"});
+      } else {
+        const avatar = gravatar.url({
+          s: 200, //size
+          r: 'pg', //rating
+          d: 'mm' //default
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password
+        })
+      }
+    })
 });
 
 module.exports = router;
