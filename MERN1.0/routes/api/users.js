ee9b46ed2f717f378571ea20d2256fd09b4167ea
Ashish Singh <ashishcodes4@gmail.com>
2018-07-03 10:41:26 +0530

    register and login success route created

--- MERN1.0/routes/api/users.js
+++ MERN1.0/routes/api/users.js
@@ -35,9 +35,46 @@ router.post('/register', (req, res) => {
           email: req.body.email,
           avatar: avatar,
           password: req.body.password
-        })
+        });
+        bcrypt.genSalt(10, (err, salt)=> {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          }); 
+        } )
       }
     })
 });
+
+//@route : /api/user/login
+//@desc: confirm user exists
+//@access: pucblic
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // check if user exists
+  User.findOne({email})
+    .then(user => {
+      //check for user
+      if(!user) {
+        return res.status(404).json({msg: "email not found"});
+      }
+      //check password
+      bcrypt.compare(password, user.password) 
+        .then(isMatch => {
+          if(isMatch) {
+            res.json({msg: "Success"})
+          } else {
+            return res.status(404).json({password:'password incorrect'});
+          }
+        })
+      
+    })
+})
 
 module.exports = router;
