53828d016714d104cf53c34d51109ed6407c0b3c
Ashish Singh <ashishcodes4@gmail.com>
2018-07-08 13:00:06 +0530

    adding model test for users & profile

--- MERN1.0/routes/api/profiles.js
+++ MERN1.0/routes/api/profiles.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+//Load profile model
+const Profile = require('../../models/Profile')
+//Load user model
+const User = require('../../models/User');
+
+
+//@route : /api/profile/tes
+//@desc: used for testing the profile route
+//@access: public
 router.get('/test', (req, res) => {
     res.json({
         msg: "profile works"
     });
 });
+
+//@route : /api/profile/
+//@desc: get current user profile
+//@access: private
+router.get('/', passport.authenticate('jwt', { session: flase }), (req, res) => {
+    
+})
 
 module.exports = router;
