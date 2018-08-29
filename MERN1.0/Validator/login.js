f14e6ce45dbd4609a832c5c399378fe5c4250f35
Ashish Singh <ashishcodes4@gmail.com>
2018-08-30 03:57:30 +0530

    login page change

--- MERN1.0/Validator/login.js
+++ MERN1.0/Validator/login.js
@@ -12,7 +12,7 @@ module.exports = function validateLoginInput (data) {
   if(validator.isEmpty(data.email)) {
       errors.email = "email can not be left blank";
   }
-  if(validator.isEmpty(data.email)) {
+  if(!validator.isEmail(data.email)) {
     errors.email = "email is invalid";
 }
   if(validator.isEmpty(data.password)) {
