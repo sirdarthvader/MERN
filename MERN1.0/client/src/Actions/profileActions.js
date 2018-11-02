47c37d83ab27084195556875df9c431244e284a6
Ashish <ashishcodes4@gmail.com>
2018-11-02 07:08:46 -0400

    setting up profile reducer and profileAction

---
+++ MERN1.0/client/src/Actions/profileActions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "./types";
+
+///Get current profile
+export const getCurrentProfile = () => dispacth => {
+  dispacth(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res => {
+      dispacth({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispacth({
+        type: GET_PROFILE,
+        payload: {}
+      });
+    });
+};
+
+//Profile Loading
+export const setProfileLaoding = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
--- MERN1.0/client/src/Reducers/index.js
+++ MERN1.0/client/src/Reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
 
 
 export default combineReducers ({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    profile: profileReducer
 });
