d75147c160ae101ca651d95504e94d3a62597b17
Ashish <ashishcodes4@gmail.com>
2018-11-02 07:29:18 -0400

    working on profile reducer and profile actions

--- MERN1.0/client/src/Reducers/profileReducer.js
+++ MERN1.0/client/src/Reducers/profileReducer.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE, PROFILE_LOADING } from "../Actions/types";
+
 const initialState = {
   profile: null,
   profiles: null,
@@ -6,8 +8,18 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_PROFILE:
+      return {
+        ...state,
+        loading: false,
+        profile: action.payload
+      };
     default:
       return state;
   }
 }
-
