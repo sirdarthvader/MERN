581b28e22bee42e52feafddb89644f23fb27d18b
Ashish Singh <ashishcodes4@gmail.com>
2018-11-06 02:37:32 +0530

    profile reducer almost done :D

--- MERN1.0/client/src/Reducers/profileReducer.js
+++ MERN1.0/client/src/Reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_LOADING } from "../Actions/types";
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from "../Actions/types";
 
 const initialState = {
   profile: null,
@@ -19,6 +19,11 @@ export default function(state = initialState, action) {
         loading: false,
         profile: action.payload
       };
+    case CLEAR_CURRENT_PROFILE: 
+    return {
+      ...state,
+      profile: null
+    }
     default:
       return state;
   }
