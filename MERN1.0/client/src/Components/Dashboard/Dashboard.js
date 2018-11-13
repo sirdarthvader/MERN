a49097369a1b63f9c9d380e7a293cccccbaa81eb
Ashish Singh <ashishcodes4@gmail.com>
2018-11-14 02:35:40 +0530

    working on dashboard, not able to detect any changes in the file system

--- MERN1.0/client/src/Components/Dashboard/Dashboard.js
+++ MERN1.0/client/src/Components/Dashboard/Dashboard.js
@@ -7,48 +7,48 @@ import { getCurrentProfile } from '../../Actions/profileActions';
 
 
 class Dashboard extends Component {
-
-    componentDidMount() {
-        this.props.getCurrentProfile();
-    }
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
 
   render() {
-    const {user} = this.props.auth;
-    const {profile, loading} = this.props.profile;
-    let dashBoardContent;
-    
-    if(user) {
-      if(profile === null || loading) {
-        dashBoardContent = <Loader />
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <Loader />;
+    } else {
+      // Check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
       } else {
-        //check if the logged in user has any profile data
-        if(Object.keys(profile).length > 0 ) {
-          dashBoardContent = <h4>TODO: Show profile information</h4>
-        } else {
-          //user is logged in buthas no profile info to be shown
-          dashBoardContent = (
-            <div>
-              <p className="text-muted lead">Welcome {user.name}</p>
-              <p>You have not created your profile yet, please add some info to get you started</p>
-              <Link to='create-profile' className='btn btn-lg btn-info'>Create Profile</Link>
-            </div>
-          );
-        }
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
       }
-    } 
+    }
 
     return (
-      <div className='dashboard'>
+      <div className="dashboard">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <h1 className="display-4">Dashboard</h1>
-              {dashBoardContent}
+              {dashboardContent}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -56,11 +56,11 @@ Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
-  auth: state.auth,
-  profile: state.profile
-})
+  profile: state.profile,
+  auth: state.auth
+});
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
