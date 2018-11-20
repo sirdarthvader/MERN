ea4aa88aaadcd5368439e27e4af477ecb7e83843
Ashish <ashishcodes4@gmail.com>
2018-11-20 06:47:46 -0500

    working on profile cred

--- MERN1.0/client/src/Components/profile/ProfileAbout.js
+++ MERN1.0/client/src/Components/profile/ProfileAbout.js
@@ -6,36 +6,32 @@ import PropTypes from 'prop-types';
 class ProfileAbout extends Component {
   render() {
     const {profile} = this.props;
+
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
     return (
       <div className='profile-about'>
       <div className="row">
             <div className="col-md-12">
               <div className="card card-body bg-light mb-3">
-                <h3 className="text-center text-info">John's Bio</h3>
-                <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident fuga cum necessitatibus blanditiis vel,
-                  officia facere porro esse numquam assumenda doloremque saepe aliquam nemo excepturi aliquid maiores! Excepturi,
-                  libero repudiandae.
+                <h3 className="text-center text-info">{profile.user.name}'s Bio</h3>
+                <p className="lead">{profile.bio}
                 </p>
                 <hr />
                 <h3 className="text-center text-info">Skill Set</h3>
                 <div className="row">
                   <div className="d-flex flex-wrap justify-content-center align-items-center">
-                    <div className="p-3">
-                      <i className="fa fa-check"></i> HTML</div>
-                    <div className="p-3">
-                      <i className="fa fa-check"></i> CSS</div>
-                    <div className="p-3">
-                      <i className="fa fa-check"></i> JavaScript</div>
-                    <div className="p-3">
-                      <i className="fa fa-check"></i> Python</div>
-                    <div className="p-3">
-                      <i className="fa fa-check"></i> C#</div>
-                  </div>
+                    {skills}
                 </div>
               </div>
             </div>
           </div>
       </div>
+    </div>
     )
   }
 }
