b5b6548adddbdee9a9ea003ed9aad63757040651
Ashish Singh <ashishcodes4@gmail.com>
2018-11-23 06:21:42 +0530

    second deploy attempt

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,26 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './Reducers';
 
 const middleware = [thunk];
 const initialState = {};
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const store = createStore(  rootReducer,
+  initialState,composeWithDevTools(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+));
 
 export default store;
+
+
+
